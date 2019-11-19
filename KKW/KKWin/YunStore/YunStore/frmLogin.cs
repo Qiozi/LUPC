4613e3cbef0c31004b1380669a897ff65087a5f1
WuTianHui <wu.th@qq.com>
2019-11-19 12:16:58 +0800

    a

--- KKW/KKWin/YunStore/YunStore/frmLogin.cs
+++ KKW/KKWin/YunStore/YunStore/frmLogin.cs
@@ -58,5 +58,10 @@ namespace YunStore
                 return;
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
