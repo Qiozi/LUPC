8ae39079ba2298b6aec991d935797de95782dc6f
WuTianHui <wu.th@qq.com>
2020-02-24 13:18:11 +0800

    d
    d

--- KKW/KKWStore/KKWStore/frmIP.Designer.cs
+++ KKW/KKWStore/KKWStore/frmIP.Designer.cs
@@ -39,10 +39,9 @@
             // button1
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Location = new System.Drawing.Point(45, 55);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(30, 33);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.Size = new System.Drawing.Size(75, 21);
             this.button1.TabIndex = 1;
             this.button1.TabStop = false;
             this.button1.Text = "用户名：";
@@ -50,10 +49,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(307, 178);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Location = new System.Drawing.Point(205, 107);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(126, 35);
+            this.button3.Size = new System.Drawing.Size(84, 21);
             this.button3.TabIndex = 3;
             this.button3.Text = "取消（&C）";
             this.button3.UseVisualStyleBackColor = true;
@@ -62,10 +60,9 @@
             // button4
             // 
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button4.Location = new System.Drawing.Point(50, 128);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Location = new System.Drawing.Point(30, 60);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(112, 35);
+            this.button4.Size = new System.Drawing.Size(75, 21);
             this.button4.TabIndex = 5;
             this.button4.TabStop = false;
             this.button4.Text = "密码：";
@@ -73,20 +70,18 @@
             // 
             // textBoxPwd
             // 
-            this.textBoxPwd.Location = new System.Drawing.Point(171, 132);
-            this.textBoxPwd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxPwd.Location = new System.Drawing.Point(114, 60);
             this.textBoxPwd.Name = "textBoxPwd";
-            this.textBoxPwd.Size = new System.Drawing.Size(262, 26);
+            this.textBoxPwd.Size = new System.Drawing.Size(176, 21);
             this.textBoxPwd.TabIndex = 4;
             this.textBoxPwd.UseSystemPasswordChar = true;
             this.textBoxPwd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxPwd_KeyDown);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(171, 178);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Location = new System.Drawing.Point(114, 107);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(128, 35);
+            this.button5.Size = new System.Drawing.Size(85, 21);
             this.button5.TabIndex = 6;
             this.button5.Text = "登入";
             this.button5.UseVisualStyleBackColor = true;
@@ -96,28 +91,26 @@
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(171, 55);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Location = new System.Drawing.Point(114, 33);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(262, 28);
+            this.comboBox1.Size = new System.Drawing.Size(176, 20);
             this.comboBox1.TabIndex = 7;
             // 
             // frmIP
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(514, 282);
+            this.ClientSize = new System.Drawing.Size(343, 169);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.textBoxPwd);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmIP";
-            this.Text = "库存管理";
+            this.Text = "库存管理-旧版";
             this.Load += new System.EventHandler(this.frmIP_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmIP_KeyDown);
             this.ResumeLayout(false);
